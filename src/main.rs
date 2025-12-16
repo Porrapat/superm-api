1827ea9c646690b0d043a6b7d17b912411cd7676
Porrapat Petchdamrongskul <porrapat.cpe20@gmail.com>
2025-12-16 11:09:54 +0700

    Add more ports

--- src/main.rs
+++ src/main.rs
@@ -5,10 +5,14 @@ use axum::{
 
 #[tokio::main]
 async fn main() {
-    // build our application with a single route
-    let app = Router::new().route("/", get(|| async { "Hello, World!" }));
+    let addr = "0.0.0.0:3000";
 
-    // run our app with hyper, listening globally on port 3000
-    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
+    let app = Router::new()
+        .route("/", get(|| async { "Hello, World!" }));
+
+    let listener = tokio::net::TcpListener::bind(addr).await.unwrap();
+
+    println!("SuperM API running at http://{}", addr);
+
     axum::serve(listener, app).await.unwrap();
 }
