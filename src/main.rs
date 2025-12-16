08bc43c54b794f84e86ef88b21c9b1c9a349dbb9
Porrapat Petchdamrongskul <porrapat.cpe20@gmail.com>
2025-12-16 11:43:50 +0700

    Add Product List page first

--- src/main.rs
+++ src/main.rs
@@ -3,11 +3,98 @@ use std::env;
 use axum::{
     routing::get,
     Router,
+    Json
 };
 
 use tower_http::{
     services::ServeDir
 };
+
+use serde::Serialize;
+
+#[derive(Serialize)]
+struct Product {
+    id: u32,
+    name: String,
+    original_price: u32,
+    final_price: u32,
+    thumbnail: String,
+}
+
+async fn products_list() -> Json<Vec<Product>> {
+    Json(vec![
+        Product {
+            id: 4,
+            name: "Apple".into(),
+            original_price: 130,
+            final_price: 100,
+            thumbnail: "http://localhost:3001/images/apple.jpg".into(),
+        },
+        Product {
+            id: 5,
+            name: "Cheese".into(),
+            original_price: 500,
+            final_price: 500,
+            thumbnail: "http://localhost:3001/images/cheese.jpg".into(),
+        },
+        Product {
+            id: 6,
+            name: "Honey".into(),
+            original_price: 500,
+            final_price: 400,
+            thumbnail: "http://localhost:3001/images/honey.jpg".into(),
+        },
+        Product {
+            id: 7,
+            name: "Sugar".into(),
+            original_price: 100,
+            final_price: 100,
+            thumbnail: "http://localhost:3001/images/sugar.jpg".into(),
+        },
+        Product {
+            id: 1,
+            name: "Tomato".into(),
+            original_price: 120,
+            final_price: 100,
+            thumbnail: "http://localhost:3001/images/tomato.jpg".into(),
+        },
+        Product {
+            id: 8,
+            name: "Almonds".into(),
+            original_price: 350,
+            final_price: 300,
+            thumbnail: "http://localhost:3001/images/almonds.jpg".into(),
+        },
+        Product {
+            id: 9,
+            name: "Bread".into(),
+            original_price: 250,
+            final_price: 200,
+            thumbnail: "http://localhost:3001/images/bread.jpg".into(),
+        },
+        Product {
+            id: 10,
+            name: "Chocolate chip cookie".into(),
+            original_price: 250,
+            final_price: 200,
+            thumbnail: "http://localhost:3001/images/cookie.jpg".into(),
+        },
+        Product {
+            id: 2,
+            name: "Pineapple".into(),
+            original_price: 200,
+            final_price: 200,
+            thumbnail: "http://localhost:3001/images/pineapple.jpg".into(),
+        },
+        Product {
+            id: 3,
+            name: "Banana".into(),
+            original_price: 50,
+            final_price: 75,
+            thumbnail: "http://localhost:3001/images/banana.jpg".into(),
+        },
+    ])
+}
 
 #[tokio::main]
 async fn main() {
@@ -17,7 +104,8 @@ async fn main() {
     let addr = format!("0.0.0.0:{}", port);
 
     let app = Router::new()
-        .route("/", get(|| async { "Hello, Wsorld!" }))
+        .route("/", get(|| async { "Hello, World!" }))
+        .route("/products-list", get(products_list))
         .nest_service("/images", ServeDir::new("images"));
 
     println!("SuperM API running at http://{}", addr);
